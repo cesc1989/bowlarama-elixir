bf098b88ae3d0896460949ca6ca512ea7f1454e8
Francisco Quintero Coronell <frajaquico@yahoo.es>
2021-03-29 15:24:49 -0500

    Produce list of a player's scores

--- lib/players.ex
+++ lib/players.ex
@@ -1,9 +1,26 @@
 defmodule Players do
   @moduledoc """
-  Map structure to hold players and their scores.
+  Structure to hold players and their scores.
   """
 
+  # players: ["Jeff", "John"]
+  # scores: [["Jeff", "10"], ["John", "3"], [], ...]
   def assign_scores_to(players, scores) do
-    {players, scores}
+    player_1 = List.first(players)
+
+    Enum.reduce(scores, [], fn(pair, acc) -> [assign_to_player(player_1, pair) | acc] end) |>
+    organize_scores()
+  end
+
+  defp assign_to_player(player, score_pair) do
+    # Estan al reves y retornando nulos
+    if List.first(score_pair) == player do
+      List.last(score_pair)
+    end
+  end
+
+  defp organize_scores(scores) do
+    Enum.reverse(scores) |>
+    Enum.reject(fn(ele) -> is_nil(ele) end)
   end
 end
