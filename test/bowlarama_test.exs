56e6bcb81e265182b53036e03e7b24f7289131d5
Francisco Quintero Coronell <frajaquico@yahoo.es>
2021-04-19 18:15:27 -0500

    Tests for Bowlarama public functions

--- test/bowlarama_test.exs
+++ test/bowlarama_test.exs
@@ -2,7 +2,11 @@ defmodule BowlaramaTest do
   use ExUnit.Case
   doctest Bowlarama
 
-  test "greets the world" do
-    assert Bowlarama.hello() == :world
+  test "results score card with existing file" do
+    assert Bowlarama.score_sheet("fixtures/scores.txt") == :ok
+  end
+
+  test "returns error when missing scores file" do
+    assert Bowlarama.score_sheet("noexisto.txt") == "File does not exist"
   end
 end
