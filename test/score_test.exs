459df0f9ab2d383d42f5f2d0e8f554c003465f15
Francisco Quintero Coronell <frajaquico@yahoo.es>
2021-04-20 20:45:04 -0500

    More test cases for Score module

--- test/score_test.exs
+++ test/score_test.exs
@@ -1,4 +1,20 @@
 defmodule ScoreTest do
   use ExUnit.Case
   doctest Score
+
+  test "raises exception when rolls are lesser than 0" do
+    assert_raise InvalidRollError, fn ->
+      Score.convert_to_numbers(["-10", "2", "3", "5"])
+    end
+  end
+
+  test "raises exception when rolls are greater than 10" do
+    assert_raise InvalidRollError, fn ->
+      Score.convert_to_numbers(["11", "2", "3", "5"])
+    end
+  end
+
+  test "returns 0 instead of F" do
+    [0 | _] = Score.convert_to_numbers(["F", "2", "3", "5"])
+  end
 end
